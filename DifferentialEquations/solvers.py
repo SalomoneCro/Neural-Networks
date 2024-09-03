43d0ae9915fcd1d152ccb2bb9e017e0d7cf54348
SalomoneCro <pedrosalomone@hotmail.com>
2024-09-03 08:20:17 -0300

    Solvers not complete yet

--- DifferentialEquations/solvers.py
+++ DifferentialEquations/solvers.py
@@ -1,101 +1,98 @@
+# -*- coding: utf-8 -*-
 import numpy as np
+import scipy as sp
+import scipy.linalg as linalg
+import matplotlib.pyplot as plt
 
+def euler(f,x,t,h,p):
+    """
+    Calcula un paso de integración del método de Euler.
 
-class solver:
+    Argumentos de entrada:
 
-    # def euler(fun, interval, x_0, h, p):
-    #     x_i = x_0
-    #     t_i = interval[0]
-    #     its =  int(np.abs(interval[0] - interval[1]) / np.abs(h))
-    #     sol = [x_0]
+        f : R^n,R -> R^n
+        x = x(t) : R^n
+        t = tiempo : R
+        h = paso de tiempo : R
+        p = parametros : R^q
 
-    #     for _ in range(its - 1):
-    #         x_i += h*fun(p,x_i,t_i)
-    #         t_i += h
-    #         sol.append(x_i)
-    #     return sol
+    Retorna aproximacion numérica de
 
-    def Runge_Kutta(fun, interval, x_0, h, p):
-        x_i = x_0
-        t_i = interval[0]
-        its =  int(np.abs(interval[0] - interval[1]) / np.abs(h))
-        sol = [x_0]
+        x(t+h) : R^n
 
-        for _ in range(its - 1):
+    según el método de Euler.
 
-            F1 = h*fun(p, t_i, x_i)
-            F2 = h*fun(p, t_i + h/2, x_i + F1/2)
-            F3 = h*fun(p, t_i + h/2, x_i + F2/2)
-            F4 = h*fun(p, t_i + h, x_i + F3)
-            x_i += (F1 + 2*F2 + 2*F3 + F4)/6
-            t_i += h
-            sol.append(x_i)
-        return sol
-    
+    # Ejemplos:
+    """
+    return x+h*f(x,t,p)
 
-    def euler_step(f,x,t,h,p):
-        """
-        Calcula un paso de integración del método de Euler.
+def integrador_ode(m,f,xa,a,b,k,p,c=lambda x,t,p:x):
+    """
+    Integra numéricamente la ODE
 
-        Argumentos de entrada:
+        dx/dt = f(x,t)
 
-            f : R^n,R -> R^n
-            x = x(t) : R^n
-            t = tiempo : R
-            h = paso de tiempo : R
-            p = parametros : R^q
+    sobre el intervalo t:[a,b] usando k pasos de integración y el método m, bajo condicion inicial x(a)=x0.
+    No es necesario que a<b.
 
-        Retorna aproximacion numérica de
+    Argumentos de entrada:
 
-            x(t+h) : R^n
+        m = metodo de integracion (ej. euler, rk2, etc.)
+        f : R^n -> R^n
+        xa = condicion inicial : R
+        a = tiempo inicial : R
+        b = tiempo final : R
+        k = num. pasos de integracion : N
+        p = parametros : R^q
+        c = función condicionante : R^n,R,p -> R^n
 
-        según el método de Euler.
+    Retorna:
 
-        # Ejemplos:
-        """
-        return x+h*f(x,t,p)
+        t : R^{k+1} , t_j = a+j*h para j=0,1,...,k
+        w : R^{n,k+1} , w_ij = x_i(t_j) para i=0,1,...,n-1 y j=0,1,...,k
 
-    def euler(f,xa,a,b,k,p,c=lambda x,t,p:x):
-        """
-        Integra numéricamente la ODE
+    donde a+k*dt = b.
+    """
+    assert k>0
+    n = len(xa)
+    h = (b-a)/k
+    w = np.zeros((n,k+1)) # Produce un array con forma y tipo especificada con los parametros,
+                          # lleno de ceros. la forma puede ser espcificada con un entero o tupla (n,k+1)
+    t = np.zeros(k+1)
+    w[:,0] = xa           # actualiza la posicion inicial (columna de indice 0) de las variables con los valores
+                          # de las condiciones iniciales
+    t[0] = a              # actualiza la posicion cero con el valor del tiempo inicial
 
-            dx/dt = f(x,t)
+    for j in range(k):    #Aca se produce la iteración en j
 
-        sobre el intervalo t:[a,b] usando k pasos de integración y el método m, bajo condicion inicial x(a)=x0.
-        No es necesario que a<b.
+        t[j+1] = t[j] + h                # iteracion tiempo
+        w[:,j+1] = m(f,w[:,j],t[j],h,p)  # iteracion de w
+        w[:,j+1] = c(w[:,j+1],t[j+1],p)  # condicion sobre w
 
-        Argumentos de entrada:
+    return t,w
 
-            m = metodo de integracion (ej. euler, rk2, etc.)
-            f : R^n -> R^n
-            xa = condicion inicial : R
-            a = tiempo inicial : R
-            b = tiempo final : R
-            k = num. pasos de integracion : N
-            p = parametros : R^q
-            c = función condicionante : R^n,R,p -> R^n
+def rk4(f,x,t,h,p):
+    """
+    Calcula un paso de integración del método de Runge Kutta orden 4.
 
-        Retorna:
+    Argumentos de entrada:
 
-            t : R^{k+1} , t_j = a+j*h para j=0,1,...,k
-            w : R^{n,k+1} , w_ij = x_i(t_j) para i=0,1,...,n-1 y j=0,1,...,k
+        f : R^n -> R^n
+        x = x(t) : R^n
+        t = tiempo : R
+        h = paso de tiempo : R
+        p = parametros : R^q
 
-        donde a+k*dt = b.
-        """
-        assert k>0
-        n = len(xa)
-        h = (b-a)/k
-        w = np.zeros((n,k+1)) # Produce un array con forma y tipo especificada con los parametros,
-                            # lleno de ceros. la forma puede ser espcificada con un entero o tupla (n,k+1)
-        t = np.zeros(k+1)
-        w[:,0] = xa           # actualiza la posicion inicial (columna de indice 0) de las variables con los valores
-                            # de las condiciones iniciales
-        t[0] = a              # actualiza la posicion cero con el valor del tiempo inicial
+    Retorna aproximacion numérica de
 
-        for j in range(k):    #Aca se produce la iteración en j
+        x(t+h) : R^n
 
-            t[j+1] = t[j] + h                # iteracion tiempo 
-            w[:,j+1] = w[:,j]+h*f(w[:,j],t[j],p)  # iteracion de w
-            w[:,j+1] = c(w[:,j+1],t[j+1],p)  # condicion sobre w
+    según el método de Runge-Kutta de orden 4.
 
-        return t,w
+    # Ejemplos:
+    """
+    k1 = f(x,t,p)
+    k2 = f(x+0.5*h*k1,t+0.5*h,p)
+    k3 = f(x+0.5*h*k2,t+0.5*h,p)
+    k4 = f(x+h*k3,t+h,p)
+    return x+h*(k1+2.0*k2+2.0*k3+k4)/6.0
