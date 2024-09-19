cbb934e60aea043434a6559bc362dbc9019fc3a3
SalomoneCro <pedrosalomone@hotmail.com>
2024-09-19 19:50:49 -0300

    Lorenz system (not working yet)

--- DifferentialEquations/solvers.py
+++ DifferentialEquations/solvers.py
@@ -38,3 +38,21 @@ def Runge_Kutta(fun, interval, x_0, h, p, act_fcnt=lambda x,t,p: x):
         t_i += h
         sol.append(x_i)
     return sol
+
+def Runge_Kutta_Sistema(fun, interval, X0, h, p, act_fcnt=lambda x,t,p: x):
+    X_i = X0
+    t_i = interval[0]
+    its =  int(np.abs(interval[0] - interval[1]) / np.abs(h))
+    sol = [X0]
+
+    for _ in range(its - 1):
+        F1 = h*fun(X_i, t_i, p)
+        F2 = h*fun(X_i + F1/2, t_i + h/2, p)
+        F3 = h*fun(X_i + F2/2, t_i + h/2, p)
+        F4 = h*fun(X_i + F3, t_i + h, p)
+        X_i += (F1 + 2*F2 + 2*F3 + F4)/6
+        X_i = act_fcnt(X_i, t_i, p)
+        t_i += h
+
+        sol.append(X_i.tolist())
+    return np.array(sol)
